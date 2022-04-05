4bcd1e90b2846bf06d33c8e5113bdeb2b3312334
pogu95 <8yd3@fresnou.org>
2022-04-05 10:33:40 -0700

    final commit

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+doctype html
+html
+head
+title= title
+link(rel='stylesheet', href='/stylesheets/style.css')
+body
+nav
+div.container
+h3 Blogzilla
+ul.nav.justify-content-end.main-nav
+li.nav-item
+a.nav-link(href='/') View Articles
+if user
+  li.nav-item
+a.nav-link(href="#") Hello #{user.first_name}
+li.nav-item
+a.nav-link(href="/logout") Logout
+else
+li.nav-item
+a.nav-link(href="/login") Login
+li.nav-item
+a.nav-link(href="/register") Register
+div.container
+block content
---
+++ models/user.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            User.belongsTo(models.Role, {
+                as: 'role',
+                foreignKey:'role_id'
+            })
+        }
+        is(role){
+            return this.role.role_name === role;
+        }
+    };
+    User.init({
+        first_name: DataTypes.STRING,
+        last_name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        role_id: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'User',
+        timestamps:false,
+        tableName:'blog_users'
+    });
+    return User;
+};
